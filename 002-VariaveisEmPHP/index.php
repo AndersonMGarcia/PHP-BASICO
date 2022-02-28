0731ba0badf1e90df3894366a25cf74d1f3907aa
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-02-28 18:10:13 -0300

    Variaveis em PHP

---
+++ 002-VariaveisEmPHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis em PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Curso de PHP para Iniciantes - by Gustavo Guanabara</h1>
+    <div>
+
+        
+
+        <?php
+        
+        $n = 4;
+        $idade = 38;
+        $nome = "Anderson Molina Garcia";
+
+        echo "$nome tem $idade anos!";
+        
+
+        
+        
+        ?>
+    </div>
+</body>
+</html>
